5bb2dfcaa3518b2642812d260f2a518cff415992
tgotwig <tgotwig@gmail.com>
2020-10-26 10:08:51 +0100

    ✅ [elixir] Add rev_str

--- elixir-cardio/lib/elixir_cardio.ex
+++ elixir-cardio/lib/elixir_cardio.ex
@@ -15,4 +15,17 @@ defmodule ElixirCardio do
   def hello do
     :world
   end
+
+  @doc """
+  Reverses a String.
+
+  ## Examples
+
+      iex> ElixirCardio.rev_str("disney")
+      "yensid"
+  """
+  def rev_str(text) when is_binary(text) do
+    text
+    |> String.reverse()
+  end
 end
