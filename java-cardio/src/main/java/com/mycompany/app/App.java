ef0eafe8d496a09c684d1bf20b6bc69f1c43b0c2
tgotwig <tgotwig@gmail.com>
2020-10-28 16:15:23 +0100

    📦 [java] Move code

--- java-cardio/src/main/java/com/mycompany/app/App.java
+++ java-cardio/src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,5 @@
 package com.mycompany.app;
 
-import java.util.stream.IntStream;
-
 /**
  * Hello world!
  *
@@ -11,15 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
-
-    public static String revStr(String string) {
-      return new StringBuilder(string).reverse().toString();
-    }
-
-    public static Boolean isPalindrome(String text) {
-      String temp  = text.replaceAll("\\s+", "").toLowerCase();
-      return IntStream.range(0, temp.length()/2)
-        .allMatch(i -> temp.charAt(i) == temp.charAt(temp.length() - i - 1));
     }
 }
--- java-cardio/src/test/java/com/mycompany/app/AppTest.java
+++ java-cardio/src/test/java/com/mycompany/app/AppTest.java
@@ -1,28 +1,32 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertEquals;
-
+import java.util.stream.IntStream;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    public static String revStr(String string) {
+      return new StringBuilder(string).reverse().toString();
+    }
     @Test
     public void shouldReverseString()
     {
-      assertEquals("yensid", App.revStr("disney"));
+      assertEquals("yensid", revStr("disney"));
     }
 
+    // ----
+
+    public static Boolean isPalindrome(String text) {
+      String temp  = text.replaceAll("\\s+", "").toLowerCase();
+      return IntStream.range(0, temp.length()/2)
+        .allMatch(i -> temp.charAt(i) == temp.charAt(temp.length() - i - 1));
+    }
     @Test
     public void shouldValidatePalindrome()
     {
-      assertEquals(true, App.isPalindrome("abcba"));
-      assertEquals(true, App.isPalindrome("ab cba"));
-      assertEquals(false, App.isPalindrome("abc"));
+      assertEquals(true, isPalindrome("abcba"));
+      assertEquals(true, isPalindrome("ab cba"));
+      assertEquals(false, isPalindrome("abc"));
     }
 }
